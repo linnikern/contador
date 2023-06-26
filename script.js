fd875745aada49e476124d265c32c037e233ab71
linnikern <linnikaoneri@icloud.com>
2023-06-25 22:12:08 -0300

    edit

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 // Data para a contagem regressiva (ano, mês - 1, dia, hora, minuto, segundo)
 var countDownDate = new Date(2023, 6, 17, 23, 59, 59).getTime()
 
-// Resto do código para a contagem regressiva
-// ...
-
 // Atualiza o contador a cada 1 segundo
 var countdown = setInterval(function () {
   // Obtém a data e hora atual
   var now = new Date().getTime()
 
-  // Calcula a diferença entre a data atual e a data da contagem regressiva
+  // diferença entre a data atual e a data da contagem regressiva
   var distance = countDownDate - now
 
-  // Calcula os dias, horas, minutos e segundos restantes
+  // dias, horas, minutos e segundos restantes
   var days = Math.floor(distance / (1000 * 60 * 60 * 24))
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
